55249a708108e7be47848c0bbd4caa17c571b24e
protin <jprotin@boutiquedubio.fr>
2026-04-15 16:37:41 +0200

    Fix contraintes version providers (align modules avec envs)

--- modules/mks/versions.tf
+++ modules/mks/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     ovh = {
       source  = "ovh/ovh"
-      version = "~> 2.0"
+      version = "~> 0.46"
     }
   }
 }
--- modules/network/versions.tf
+++ modules/network/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
-      version = "~> 3.0"
+      version = "~> 2.1"
     }
   }
 }
